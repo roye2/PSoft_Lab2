b8f796a4dac1dd6857b107abfb6fe3d14b06eeb5
roye2 <07eeroy@gmail.com>
2026-09-09 18:25:40 -0400

    fix syntax

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
-public class HelloWorld {
+public class Main {
     
-    public static void main(String[] args)
-    {        
+    public static void main(String[] args) {
         System.out.println("Hello, World");
     }
 }
